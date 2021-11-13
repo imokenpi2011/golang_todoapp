a7f69c66b678836db26f5d90b774944f1791200b
Kento Nakashima <imokenpi2011@gmail.com>
2021-11-14 02:46:40 +0900

    設定ファイル(ini)の読み込み処理を作成

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+// 設定一覧を指定
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+	Static    string
+}
+
+//Configインスタンスを定義
+var Config ConfigList
+
+//初期処理
+func init() {
+	//設定の読み込み処理
+	LoadConfig()
+}
+
+//設定を読み込む
+func LoadConfig() {
+	// 設定を記載したiniファイルを読み込む
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//読み込んだ値を設定する
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		DbName:    cfg.Section("db").Key("name").String(),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module golang_todoapp
+
+go 1.17
+
+require (
+	gopkg.in/go-ini/ini.v1 v1.64.0
+	honnef.co/go/tools v0.2.2
+)
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+)
