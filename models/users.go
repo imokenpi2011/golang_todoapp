82c246e4b1ee3b7813d0d8eb5bd7fbef03ef682d
anthem-landing18 <imokenpi2011@gmail.com>
2021-11-14 03:37:42 +0900

    ユーザー情報取得処理を作成

--- models/users.go
+++ models/users.go
@@ -37,3 +37,23 @@ func (u *User) CreateUser() (err error) {
 	}
 	return err
 }
+
+//ユーザー情報を取得する
+func GetUser(id int) (user User, err error) {
+	//ユーザーインスタンスを宣言
+	user = User{}
+
+	//SQL文を設定
+	cmd := `select id, uuid, name, email, password, created_at from users where id = ?`
+
+	//取得処理を実行
+	err = Db.QueryRow(cmd, id).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt,
+	)
+	return user, err
+}
