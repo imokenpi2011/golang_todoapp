c12d61fa4a558c83c9a272ef44aab64a854d869c
anthem-landing18 <imokenpi2011@gmail.com>
2021-11-15 22:58:02 +0900

    サインアップページでのPOST処理を作成

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,10 +1,36 @@
 package controllers
 
 import (
+	"golang_todoapp/app/models"
+	"log"
 	"net/http"
 )
 
-//top画面に飛ばす
+//サインアップ画面の処理
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "public_navbar", "signup")
+	//GETの時に実行する
+	if r.Method == "GET" {
+		generateHTML(w, "Hello", "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		//POSTの時に実行する
+		//入力した値を受け取る
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		//インスタンスを生成
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		//ユーザーを作成する
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+
+		//登録したらトップ画面に遷移する
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
 }
