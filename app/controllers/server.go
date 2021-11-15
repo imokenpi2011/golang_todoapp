4b46d01d20c5cc1d12377a62ac82f55e9f6812bb
anthem-landing18 <imokenpi2011@gmail.com>
2021-11-16 01:50:41 +0900

    セッションの検証メソッドを作成

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"golang_todoapp/app/models"
 	"golang_todoapp/config"
 	"net/http"
 	"text/template"
@@ -18,6 +19,20 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 	//レイアウトを明示的に読み込む
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", data)
+}
+
+//セッションを検証する
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	//クッキーを取得する
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = models.Session{UUID: cookie.Value}
+		//セッションを検証する
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid session")
+		}
+	}
+	return sess, err
 }
 
 //サーバーを起動する
