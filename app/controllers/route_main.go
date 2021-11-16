b5046bfda67b4ed9e40c90dee48b7eb7005ceb58
anthem-landing18 <imokenpi2011@gmail.com>
2021-11-16 21:02:48 +0900

    タスク表示画面にログイン中ユーザーのタスクを表示する様変更

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -20,12 +21,20 @@ func top(w http.ResponseWriter, r *http.Request) {
 //タスク表示画面の制御
 func index(w http.ResponseWriter, r *http.Request) {
 	//セッションを取得する
-	_, err := session(w, r)
+	sess, err := session(w, r)
 	if err != nil {
 		//セッションが存在しない場合はトップページに遷移する
 		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
+		//ログイン中のユーザー情報を取得する
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		//ユーザーに紐づくタスクを取得する
+		todos, _ := user.GetTodosByUser()
+		user.Todos = todos
 		//セッションが存在する場合はタスク表示画面に遷移する
-		generateHTML(w, nil, "layout", "private_navbar", "index")
+		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
 }
